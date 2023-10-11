fb04c48d988f1f04be19a9821006ca970e75fef8
Evan BOISSONNOT <evan@boissonnot.fr>
2023-10-11 10:40:10 +0200

    feat: test deplacement

---
+++ SdA.Tests.UI.Deplacements/Program.cs
@@ -0,0 +1,28 @@
+﻿// a mettre static dans une classe Proxy
+using SdA.Game.Libs.Models.GamePlay.Characters;
+using SdA.Game.Libs.Models.GamePlay.Characters.Enemies;
+
+Random random = new Random();
+
+List<Character> list = new()
+{
+    new Orc("Orc"),
+    new Urukhai("U"),
+    new Goblin()
+};
+
+Console.WriteLine("-----> Avant déplacement -----");
+list.ForEach(character =>
+{
+    Console.WriteLine($"{character.Position.X} / {character.Position.Y}");
+});
+
+Console.WriteLine("-----> Après déplacement -----");
+list.ForEach(character =>
+{
+    character.SeDeplacer(new(random.Next(0, 100), random.Next(0, 100)));
+});
+list.ForEach(character =>
+{
+    Console.WriteLine($"{character.Position.X} / {character.Position.Y}");
+});
