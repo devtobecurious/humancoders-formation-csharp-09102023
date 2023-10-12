936b7a0e395339206f364c67f4f6ddf95e84c95b
Evan BOISSONNOT <evan@boissonnot.fr>
2023-10-12 09:45:32 +0200

    feat: product et delegue de tva

---
+++ Sda.Invoices.Libs.Models/GameProduct.cs
@@ -0,0 +1,34 @@
+﻿namespace Sda.Invoices.Libs.Models;
+
+public delegate decimal GetTva();
+
+public class GameProduct
+{
+    private readonly GetTva getTva;
+    private decimal prixHT;
+    private decimal prixTTC;
+
+    public GameProduct(GetTva getTva)
+    {
+        this.getTva = getTva;
+    }
+
+    // Première piste
+    // public decimal PrixTTC => (1 + this.getTva()) * this.PrixHT;
+    public decimal PrixTTC
+    {
+        get => this.prixTTC;
+        private set => this.prixTTC = value;
+    }
+
+
+    public decimal PrixHT
+    {
+        get => this.prixHT;
+        set
+        {
+            this.prixHT = value;
+            this.prixTTC = (1 + this.getTva()) * value;
+        }
+    }
+}
